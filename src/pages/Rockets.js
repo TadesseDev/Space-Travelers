f224ea9f2be8a403734dbb1e5c709dcb90ff0ed2
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 00:23:34 +0300

    Add state selector for the Rocket component

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
 export default function Rockets() {
+
+  const Rocket = useSelector(state => state.Rockets);
   return (
-    <div>Rockets</div>
+    <div>Rockets{Rocket.length}</div>
   )
 }
