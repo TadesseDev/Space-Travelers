0c38005f640532634df61b2c630b0600f332c152
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-09 21:35:35 +0300

    Create a navbar contents

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 return (
-  <nav style={navStyle}><NavLink to="/Profile" style={lstLink}>My Profile</NavLink></li>
-      </ul >
-    </nav >
-  );
+  <nav style={navStyle}>
+    <div id="logo">
+      <span id="logo-img">
+      </span>
+      <h1 id="logo-text">
+        Space Travelers' Hubs
+      </h1>
+    </div>
+    <ul style={ulStyle}>
+      <li><NavLink to="/" style={navLinkStyle}>Rockets</NavLink></li>
+      <li><NavLink to="/Missions" style={navLinkStyle}>Missions</NavLink></li>
+      <li><NavLink to="/Profile" style={lstLink}>My Profile</NavLink></li>
+    </ul>
+  </nav>
+);
 };
 
 export default Navbar;
