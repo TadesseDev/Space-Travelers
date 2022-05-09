edeff4e5e30fea5e9c01c62c9db661d9e0457f96
Alexr16 <j.alex-1996@hotmail.com>
2022-05-09 17:47:49 -0500

    Create Missions table

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getMissions } from '../Redux/Missions/Mission';
+import Mission from '../components/Mission';
 
 export default function Missions() {
   const dispatch = useDispatch();
+  const Missions = useSelector((state) => state.Missions);
   useEffect(() => {
     dispatch(getMissions());
   }, []);
 
   return (
-    <div>Missions</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Missions</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Missions.map((mission) => (
+            <Mission
+              key={mission.mission_id}
+              mission={mission}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
