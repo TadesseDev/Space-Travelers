2fe1d72f9c785585b4a5e02c478c6bb12eb52b0d
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 00:01:21 +0300

    Add a loger to a store

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,12 +2,14 @@ import rocketReducer from '../Redux/Rockets/Rocket';
 import { createStore, applyMiddleware } from 'redux';
 import { combineReducers } from 'redux';
 import thunk from 'redux-thunk'
+import logger from 'redux-logger';
+
 const rootReducer = combineReducers({
   Rockets: rocketReducer,
 });
 
 const store = createStore(
-  rootReducer, applyMiddleware(thunk)
+  rootReducer, applyMiddleware(thunk, logger)
 );
 
 export default store;
