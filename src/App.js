dc076ae32b07dd628bbbc3deba20a6300c0003b8
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-09 21:51:52 +0300

    Create working routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import './App.scss';
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router } from 'react-router-dom'
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () =>
   <Router>
     <Navbar />
-    {/* <Routes>
-    </Routes> */}
+    <Routes>
+      <Route path='/' element={<Rockets />} />
+      <Route path='/Missions' element={<Missions />} />
+      <Route path='/Profile' element={<Profile />} />
+    </Routes>
   </Router>
 
 
