5a006ecf43cd3c24ac2577da323da3d8fca6ae98
Alexr16 <j.alex-1996@hotmail.com>
2022-05-10 14:29:27 -0500

    Create Navbar renders test

---
+++ src/__tests__/navbar.test.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import TestRenderer from 'react-test-renderer';
+import Navbar from '../components/NavBar';
+
+it('Check if Navbar page renders correctly', () => {
+  const tree = TestRenderer.create(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>,
+  );
+  expect(tree).toMatchSnapshot();
+});
