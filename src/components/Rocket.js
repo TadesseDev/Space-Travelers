0b7f18e4a932a778085143651aa53198555072bc
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 11:55:51 +0300

    Update Rocket parts into bootstrap component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import { Row, Col, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Rocket({ id, name, description, image, reserved }) {
   return (
-    <div className="rocket-container">
-      <img src={image} alt={name} />
-      <div className="rocket-description">
+    <Row className="rocket-container">
+      <Col md={3} sm={4}>
+        <img src={image} alt={name} width="100%" />
+      </Col>
+      <Col className="rocket-description">
         <h3>{name}</h3>
         <p>{description}</p>
         {reserved ?
-          (<button className="reserved" id={id}>reserved</button>) :
-          (<button className="not-reserved" id={id}>not reserved</button>)}
-      </div>
-    </div>
+          (<Button primary className="reserved" id={id}>reserved</Button>) :
+          (<Button warning className="not-reserved" id={id}>not reserved</Button>)}
+      </Col>
+    </Row>
   )
 }
