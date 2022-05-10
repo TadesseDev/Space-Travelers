8361e438966acb247f7b1fdb7e93f2205f8ed54e
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 12:39:13 +0300

    Add proper bootstrap spacng and style

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -3,16 +3,16 @@ import { Row, Col, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Rocket({ id, name, description, image, reserved }) {
   return (
-    <Row className="rocket-container">
-      <Col md={3} sm={4}>
+    <Row className="rocket-container align-items-center mt-md-3">
+      <Col sm={5} md={4} lg={3}>
         <img src={image} alt={name} width="100%" />
       </Col>
       <Col className="rocket-description">
         <h3>{name}</h3>
         <p>{description}</p>
         {reserved ?
-          (<Button primary className="reserved" id={id}>reserved</Button>) :
-          (<Button warning className="not-reserved" id={id}>not reserved</Button>)}
+          (<Button className="reserved" id={id}>reserved</Button>) :
+          (<Button variant="outline-dark" className="not-reserved" id={id}>not reserved</Button>)}
       </Col>
     </Row>
   )
