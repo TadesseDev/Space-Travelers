9c5032a5454fd7cd35fdef90baf35eaaa051ccce
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 11:17:03 +0300

    Display rocket detail

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-export default function Rocket() {
+export default function Rocket({ id, name, description, image, reserved }) {
   return (
-    <div>Rocket</div>
+    <div className="rocket-container">
+      <img src={image} alt={name} />
+      <div className="rocket-description">
+        <h3>{name}</h3>
+        <p>{description}</p>
+        {reserved ?
+          (<button className="reserved" id={id}>reserved</button>) :
+          (<button className="not-reserved" id={id}>not reserved</button>)}
+      </div>
+    </div>
   )
 }
