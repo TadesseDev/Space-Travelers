c5c89d188c0b0808fab7adca116fa3afff99c5fe
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 20:30:59 +0300

    Import react-bootstrap components

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import { Table, Row, Container, Col } from 'react-bootstrap';
+import { useSelector } from 'react-redux'
 export default function Profile() {
+  const reservedRockets = useSelector(store => store.Rockets.filter(rocket => rocket.reserved));
+  console.log(reservedRockets);
   return (
-    <div>Profile</div>
   )
 }
