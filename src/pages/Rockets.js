d4ab57e2fdb9cb5ca37519a9e161a93fd44be44a
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-10 11:16:33 +0300

    Add Rocket detail in rocket.js

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import Rocket from '../components/Rocket'
+
 export default function Rockets() {
 
-  const Rocket = useSelector(state => state.Rockets);
+  const rockets = useSelector(state => state.Rockets);
   return (
-    <div>Rockets{Rocket.length}</div>
+    <div>
+      {rockets.map((rocket) =>
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.rocket_name}
+          description={rocket.description}
+          image={rocket.flickr_images}
+          reserved={rocket.reserved}
+        />)}
+    </div>
   )
 }
