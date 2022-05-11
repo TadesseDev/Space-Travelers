9fd8c792a07c9730771fcb166f5405b6e4404d11
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-11 12:53:21 +0300

    Cleanup Rocket clean file

--- src/__tests__/Rockets.test.js
+++ src/__tests__/Rockets.test.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import store from '../Redux/configureStore';
 import * as RocketCall from '../Redux/Rockets/Rocket'
 import { rocketData, STORE_ROCKETS, TOGGLE_RESERVATION } from '../__mocks__/MockRocketData'
-const addRockets = jest.spyOn(RocketCall, 'addRockets')
+const addRocketsMock = jest.spyOn(RocketCall, 'addRockets')
   .mockImplementation(() => ({ type: STORE_ROCKETS, payload: rocketData }));
-// const addRockets = jest.fn(() => ({ type: STORE_ROCKETS, payload: rocketData }));
+
+
 describe('Render the APP component and check for initial Rocket state', () => {
   // we need to render the App component because 
   // Rockets are being loaded from APP component
@@ -15,22 +16,22 @@ describe('Render the APP component and check for initial Rocket state', () => {
       <App />
     </Provider>
   );
+  expect(addRocketsMock).toBeCalled()
 
-  //assert addRocket mock is working
-  expect(addRockets).toBeCalled()
-  test('Assert initial rocket list is empty', () => {
+  test('Assert Rockets component is loaded', () => {
     const rocketsComponent = screen.queryAllByTestId('list-of-rockets')
     expect(rocketsComponent.length).toBe(1);
   });
 })
 
-describe('Test rocket with mock API call', () => {
-  const storeState = store.getState();
-  expect(storeState.Rockets.length).toBe(4);
-  console.log(storeState);
+describe('Test Rocket with mock API call', () => {
+  //assert addRocket mock is working
+  test('Assert store is updated with Rockets data', () => {
+    const storeState = store.getState();
+    expect(storeState.Rockets.length).toBe(4);
+    console.log(storeState);
+  })
 })
-
-
 
 afterEach(() => {
   jest.clearAllMocks();
