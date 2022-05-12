4b4acf464dcf3081291e8c5ac7ac8f5cb7b1b422
Alexr16 <j.alex-1996@hotmail.com>
2022-05-12 11:58:56 -0500

    Add cancel reservation button to the profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {
-  Table, Row, Container, Col,
+  Table, Row, Container, Col, Button,
 } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { reserveMission } from '../Redux/Missions/Mission';
+import { toggleReservedAction } from '../Redux/Rockets/Rocket';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Profile() {
   const reservedRockets = useSelector((store) => store.Rockets.filter((rocket) => rocket.reserved));
@@ -16,6 +18,12 @@ export default function Profile() {
     const reserve = event.target.getAttribute('data-reserved');
     const missionId = event.target.getAttribute('data-id');
     dispatch(reserveMission(missionId, reserve));
+  };
+
+  const toggleReservation = (event) => {
+    const reserved = event.target.getAttribute('data-reserved');
+    const rocketId = event.target.getAttribute('data-id');
+    dispatch(toggleReservedAction(rocketId, reserved));
   };
 
   return (
@@ -54,7 +62,12 @@ export default function Profile() {
             <tbody>
               {reservedRockets.map((rocket) => (
                 <tr key={rocket.id}>
-                  <td>{rocket.rocket_name}</td>
+                  <td>
+                    {rocket.rocket_name}
+                    <div>
+                      <Button variant="outline-dark " className="reserved text-capitalize" id={rocket.id} data-id={rocket.id} data-reserved={0} onClick={toggleReservation}>cancel reservation</Button>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
