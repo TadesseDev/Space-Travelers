8d0b90785123421556cd82a659559807db28ba33
Alexr16 <j.alex-1996@hotmail.com>
2022-05-12 11:51:24 -0500

    Add leave mission button to the profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,12 +2,21 @@ import React from 'react';
 import {
   Table, Row, Container, Col,
 } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveMission } from '../Redux/Missions/Mission';
 
 export default function Profile() {
   const reservedRockets = useSelector((store) => store.Rockets.filter((rocket) => rocket.reserved));
   const reservedMissions = useSelector((state) => state.Missions
     .filter((mission) => mission.reserved));
+
+  const dispatch = useDispatch();
+
+  const reserve = (event) => {
+    const reserve = event.target.getAttribute('data-reserved');
+    const missionId = event.target.getAttribute('data-id');
+    dispatch(reserveMission(missionId, reserve));
+  };
 
   return (
     <Container style={{ margin: '2vw 5vw' }}>
@@ -18,7 +27,22 @@ export default function Profile() {
             <tbody>
               {reservedMissions.map((mission) => (
                 <tr key={mission.id}>
-                  <td>{mission.mission_name}</td>
+                  <td>
+                    {mission.mission_name}
+                    <div>
+                      <button
+                        className="leave"
+                        type="button"
+                        label="leave Mission"
+                        id={mission.mission_id}
+                        data-id={mission.mission_id}
+                        data-reserved={0}
+                        onClick={reserve}
+                      >
+                        Leave Mission
+                      </button>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
